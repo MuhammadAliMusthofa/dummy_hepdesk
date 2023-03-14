6a0257bb04221ac9cc1248e5e6ba3350e28ab822
adekunia <muhammadalimusthofa@gmail.com>
2023-03-14 14:36:13 +0700

    add riwayat keluhan page

--- resources/views/users/riwayat_keluhan.blade.php
+++ resources/views/users/riwayat_keluhan.blade.php
@@ -17,6 +17,14 @@
         #action_page a{
             color:black;
         }
+
+        #keluhan {
+            background-color: #98B7C7;
+
+        }
+        button{
+            width:100%;
+        }
     </style>
 </head>
 <body bg-secondary>
@@ -28,8 +36,12 @@
         </div>
 
         <div id="action_page" class=" d-flex align-items-end mr-3 " >
-            <a class="pr-2" href=""><i class="fas fa-cog">Pengaturan</i></a>
-            <a href=""><i class="fas fa-sign-out-alt">Keluar</i></a>
+            <a class="pr-2" href="">
+                <i class="fas fa-cog mr-3">Pengaturan</i>
+            </a>
+            <a  href="">
+                <i class="fas fa-sign-out-alt mr-2">Keluar</i>
+            </a>
         </div>
     </div>
     <h6 class="ml-3 mb-5">Beranda / Layanan Helpdesk / Riwayat </h6>
@@ -39,20 +51,72 @@
             <h5 class="card-title">Riwayat Keluhan</h5>
             <form action="">
 
-                <div class="input-group mb-3 col-md-4">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                      <span class="input-group-text" id="basic-addon2"><i class="fas fa-search"></i></span>
+                <div  class="input-group mb-3 col-md-4 p-0">
+                    <input id="keluhan" type="text" class="form-control" placeholder="Cari keluhan" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <div class="input-group-append ">
+                      <span class="input-group-text" id="basic-addon2" style="background-color:#98B7C7;"><i class="fas fa-search"></i></span>
                     </div>
                   </div>
             </form>
             
         </div>
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+        <table class="table table-striped text-center">
+            <thead>
+              <tr>
+                <th scope="col">Id Ticket</th>
+                <th scope="col">Tanggal</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Email</th>
+                <th scope="col">Departemen</th>
+                <th scope="col">Detail</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">00121</th>
+                <td>07/12/2022</td>
+                <td>Erly Krisnanik</td>
+                <td>erlykrisnanik@upnvj.ac.id</td>
+                <td>Dosen</td>
+                <td ><i class="fas fa-file btn btn-secondary"></i></td>
+                
+                <td> <button class="btn btn-success">Selesai</button>  </td>
+              </tr>
+              <tr>
+                <th scope="row">00121</th>
+                <td>07/12/2022</td>
+                <td>Erly Krisnanik</td>
+                <td>erlykrisnanik@upnvj.ac.id</td>
+                <td>Dosen</td>
+                <td ><i class="fas fa-file   btn btn-secondary"></i></td>
+
+                <td > <button class="btn btn-danger">  Dibatalkan</button></td>
+              </tr>
+              <tr>
+                <th scope="row">00121</th>
+                <td>07/12/2022</td>
+                <td>Erly Krisnanik</td>
+                <td>erlykrisnanik@upnvj.ac.id</td>
+                <td>Dosen</td>
+                <td ><i class="fas fa-file btn btn-secondary"></i></td>
+                
+                <td > <button class="btn btn-warning">Berlangsung</button></td>
+
+              </tr>
+              <tr>
+                <th scope="row">00121</th>
+                <td>07/12/2022</td>
+                <td> Erly Krisnanik</td>
+                <td>erlykrisnanik@upnvj.ac.id</td>
+                <td>Dosen</td>
+                <td ><i class="fas fa-file btn btn-secondary"></i></td>
+                <td ><button class="btn btn-primary">Menunggu</button></td>
+
+              </tr>
+            
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
