fa11f17c8a29babb85272546c6648239fe6bf117
nurzaman-now <imannj69@gmail.com>
2023-03-14 10:16:52 +0700

    update jumlah antrian

--- resources/views/admin/antrianPage.blade.php
+++ resources/views/admin/antrianPage.blade.php
@@ -1,6 +1,6 @@
-{{--  <div class="p-4 pb-5" style="background-color: #D6E2E9">
-</div>  --}}
-<div class="d-flex align-items-center flex-column justify-content-center h-80">
+<div class="p-4 pb-5" style="background-color: #D6E2E9">
+</div>
+<div class="d-flex align-items-center flex-column justify-content-center h-100" style="height: 200px;">
   <div class="text-center">
     <h1>15 Antrian Menunggu</h1>
     <h3>Mari melayani sepenuh hati</h3>
