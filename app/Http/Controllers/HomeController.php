37939c4f28668d8019f18b008362ab9ae085b7a5
1706_ R Fajri Nandipinto <rfajrin@students.amikom.ac.id>
2023-03-15 13:38:02 +0700

    ssd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
         if (
             Auth::user()->role == 0
         ) {
-            return view('user.sdid');
+            return view('user.sdid2');
         } elseif (Auth::user()->role == 1) {
             return view('users.dashboard');
         }
